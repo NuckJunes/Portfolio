f60c1412f7ecd801647af36c3ea8d77476d3cadf
Nick Jones <jonesn5@miamioh.edu>
2024-07-09 13:00:53 -0400

    Added initial files, added github api for profile pic and bio.

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function getGithubData() {
+    fetch("https://api.github.com/users/NuckJunes", {
+        method: "GET"
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector("#github-image").src = data.avatar_url;
+        document.querySelector("#github-bio").innerText = "Github Bio";
+    }
+    );
+
+}
+
+getGithubData();
